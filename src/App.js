f55811cf8a17cd28000b886343ec65aa8f00b638
PugazhendhiThangaprakasam <pugazhendhi.thangaprakasam@gmail.com>
2023-11-15 15:27:58 +0530

    day 3

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Greeting from "./Day2/clswork";
+import ToggleState from "./Day3/Pah";
 import MyClassComponent from "./components/ClassComponent";
 import MyFunctionComponent from "./components/functionComponent";
 
@@ -5,8 +7,10 @@ import MyFunctionComponent from "./components/functionComponent";
 function App() {
   return (
     <div>
-      <MyClassComponent />
+      {/* <MyClassComponent />
       <MyFunctionComponent />
+      <Greeting/> */}
+      <ToggleState/>
     </div>
   );
 }
